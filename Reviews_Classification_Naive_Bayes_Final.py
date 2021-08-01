2e45ad92f665112e5fea81f79227277707bc897c
nehathaware <81456965+nehathaware@users.noreply.github.com>
2021-08-01 17:02:50 +0530

    Update Reviews_Classification_Naive_Bayes_Final.py

--- Reviews_Classification_Naive_Bayes_Final.py
+++ Reviews_Classification_Naive_Bayes_Final.py
@@ -15,35 +15,14 @@ import pickle
 
 def rating():
     data = pd.read_csv("amazon_alexa.csv")
-    
-    
     data.rename(columns={'verified_reviews': 'text'},inplace=True)
-    
-
-    
-    data.isnull().sum()
-    
-   
     Totalrating=data["rating"].count()
-     
-    
     onestar=data.loc[data['rating'] == 1].count()
-    
-    
     twostar=data.loc[data['rating'] == 2].count()
-    
-      
-    
     threestar=data.loc[data['rating'] == 3].count()
-    
-    
-    
     fourstar=data.loc[data['rating'] == 4].count()
-    
-    
     fivestar=data.loc[data['rating'] == 5].count()
-    
-    
+ 
     d=dict();
     d[' 1 : Count of 1 star rating']=onestar[0]
     d[' 2 : Count of 2 star rating'] = twostar[0]
@@ -75,28 +54,19 @@ def reviewfunc(inputvalue):
     clean = lambda x: clean_text(x)
      
     data['text'] = data.text.apply(clean)
-    data.text
-    
-
+ 
     freq = pd.Series(' '.join(data['text']).split()).value_counts()[:20] # for top 20
-    freq
-    
     
     #removing stopwords
     from nltk.corpus import stopwords
     stop = stopwords.words('english')
     data['text'] = data['text'].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
     new_data=data['text']
-    
-
     #word frequency after removal of stopwords
     freq_Sw = pd.Series(' '.join(data['text']).split()).value_counts()[:20] # for top 20
-    freq_Sw
-    
     
     string_Total = " ".join(new_data)
-    
-
+ 
     import matplotlib.pyplot as plt
     # get_ipython().run_line_magic('matplotlib', 'inline')
     from wordcloud import WordCloud, STOPWORDS
@@ -109,10 +79,6 @@ def reviewfunc(inputvalue):
         # No axis details
         plt.axis("off");
     
-    
-    # In[20]:
-    
-    
     stopwords = STOPWORDS
     stopwords.add('will')
     stopwords.add('im')
@@ -121,28 +87,13 @@ def reviewfunc(inputvalue):
     # Plot
     plot_cloud(wordcloud)
     
-    
-    
-    
     data.drop(["variation", "date","rating"], axis = 1, inplace = True)
-    
-    
-    
     
     from sklearn.feature_extraction.text import CountVectorizer
     vectorizer = CountVectorizer(stop_words='english',max_features=3000, binary=True)
     all_features = vectorizer.fit_transform(data.text)
-    all_features.shape
-    
-    
-    # In[25]:
-    
     
     vectorizer.vocabulary_
-    
-    
-    # In[26]:
-    
     
     from sklearn.model_selection import train_test_split
     review_train,review_test = train_test_split(data,test_size=0.3)
@@ -151,79 +102,40 @@ def reviewfunc(inputvalue):
     y_train=review_train.feedback
     y_test=review_test.feedback
     X_train_vect = vectorizer.fit_transform(X_train)
-    print(X_train.shape)
-    print(y_train.shape)
-    print(X_test.shape)
-    print(y_test.shape)
+    #print(X_train.shape)
+    #print(y_train.shape)
+    #print(X_test.shape)
+    #print(y_test.shape)
     
     
     # ## Train dataset balancing
-    
-    # In[27]:
-    
     
     from imblearn.over_sampling import SMOTE
     sm = SMOTE()
     X_train_res, y_train_res = sm.fit_resample(X_train_vect, y_train)
     unique, counts = np.unique(y_train_res, return_counts=True)
     print(list(zip(unique, counts)))
-    y_train_res
-    X_train_res
-    
     
     # ## Model Building
-    
-    # In[28]:
-    
-    
+   
     from sklearn.naive_bayes import MultinomialNB
     nb = MultinomialNB()
     nb.fit(X_train_res, y_train_res)
     nb.score(X_train_res, y_train_res)
     train_pred = nb.predict(X_train_res)
-    train_pred
     
-    
-    # In[29]:
-    
-    
-    from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, precision_score, recall_score
-    print("Accuracy: {:.2f}%".format(accuracy_score(y_train_res, train_pred) * 100))
+    from sklearn.metrics import accuracy_score
+    #print("Accuracy: {:.2f}%".format(accuracy_score(y_train_res, train_pred) * 100))
 
-
-    # In[30]:
-    
-    
     X_test_vect = vectorizer.transform(X_test)
     y_pred = nb.predict(X_test_vect)
-    y_pred
-    
-    
-    # In[31]:
-    
-    
-    from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, precision_score, recall_score
-    print("Accuracy: {:.2f}%".format(accuracy_score(y_test, y_pred) * 100))
-    
-    
-    # In[32]:
-    
+    #print("Accuracy: {:.2f}%".format(accuracy_score(y_test, y_pred) * 100))
     
     ## Testing for overall dataset
     
-    
-    # In[33]:
-    
-    
     X_overall_vect = vectorizer.transform(data.text)
     y_pred_overall = nb.predict(X_overall_vect)
-    y_pred_overall
-    
-    
-    # In[34]:
-    
-    
-    print("Accuracy: {:.2f}%".format(accuracy_score(data.feedback, y_pred_overall) * 100))
+    #print("Accuracy: {:.2f}%".format(accuracy_score(data.feedback, y_pred_overall) * 100))
     
     a = sum(y_pred_overall==0)
     b = sum(y_pred_overall==1)
@@ -245,8 +157,7 @@ def reviewfunc(inputvalue):
             message="Negative Sentiment"
         elif predicted_data ==1:
             message="Positive Sentiment"
-            
-        
+                
     else:
         message="No value"
         
